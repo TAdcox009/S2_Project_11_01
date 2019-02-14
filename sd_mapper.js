df316ab7d974086f0b0d733fd386705ec1f754a1
Unknown <TAdcox009@west-mec.org>
2019-02-13 17:37:06 -0700

    final commit
    Added comments, final commit, ready for review.

--- sd_mapper.js
+++ sd_mapper.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// This makes it use strict javascript.
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -11,21 +11,21 @@ Date:   2-12-19
 
 */
 
-
-// 5. Declare a variable named thistime containing a Date object for February 3, 2018 at 3:15:28 a.m.
-// 6. Use the toLocaleString() method to save the text of the thisTime variable in the timestr variable.
-// 7. Change the inner HTML code of the page element with the ID timestamp to the value of the timeStr variable.
-// 8. Next, you will determine which sky map to show in the web page. First, create a variable named thisHour, using the getHours() method to extract the hour value from the thisTime variable.
-// 9. Create a variable named thisMonth using the getMonth() method to extract the month number from the thisTime variable.
-// 10. The number of the map to use with the given hour and month is calculated with the formula (2×month + hour) % 24
-// where month is the value of the thisMonth variable and hour is the value of the thisHour variable. Store the value of this formula in the mapNum variabl
-
+// A variable named thistime adds a different date
+// The second variable saves the text of the previous variable
+// In the HTML there is a element with the ID timestamp and its going to change its value to the same as the variable named timeStr
+// Creates a variable named thisHour, using the getHours() method to get the hour value from the thisTime variable.
+//  Creates a variable named thisMonth, using the getMonth() method to get the month value from the thisTime variable.
 var thisTime = new Date();
 var timeStr = thisTime.toLocaleString();
 document.getElementById("timeStamp").innerHTML = timeStr;
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
 
+// The number of the map to tell which image to use with the given hour and month is calculated with the formula (2×month + hour) % 24
+// where month is the value of the thisMonth variable and hour is the value of the thisHour variable. The values of the formula is being stored in the mapNum variable
+
+// There is a variable named imgstr that stores the following text string <img src = 'sd_skyMap.png' / > where "Map" is the value of the mapNum variable, and when its at a certian time the image will change. For the page element with the ID planisphere there is the value of the imgStr variable directly after the element
 var mapNum = ((2 * thisMonth + thisHour) % 24);
 var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
 document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
